cb5d48dad4d61ea1f06a925e375763cec700e917
Elizabete Silva <elizabete.silvapaula@gmail.com>
2022-02-25 17:52:09 -0800

    Create css style for comment-list

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -4,7 +4,7 @@ import { v4 as uuid } from "uuid";
 function CommentList({ comments }) {
   return (
     <>
-      <ul className="comments__list">
+      <ul className="comments">
         {comments.map((comment) => (
           <li className="comments__item" key={uuid()}>
             <div className="comments__content">
@@ -12,7 +12,7 @@ function CommentList({ comments }) {
                 <div className="avatar"></div>
               </div>
               <div className="comments__info">
-                <div className="comments__info_wrapper">
+                <div className="comments__info__wrapper">
                   <p className="comments__info__name">{comment.name}</p>
                   <p className="comments__info__date">
                     {new Date(comment.timestamp).toLocaleDateString()}
