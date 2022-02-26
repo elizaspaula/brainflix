cecda65d6e9883fc595d614ddd6c852189b8bfb0
Elizabete Silva <elizabete.silvapaula@gmail.com>
2022-02-26 12:27:02 -0800

    Adjust conversations form tablet layout

--- src/components/CommentsForm/CommentsForm.jsx
+++ src/components/CommentsForm/CommentsForm.jsx
@@ -11,28 +11,30 @@ function CommentsForm() {
       </div>
 
       <div className="conversations__group">
-        <label className="conversations__label" for="message">
+        <p className="conversations__label" for="message">
           Join the Conversation
-        </label>
-        <textarea
-          id="message"
-          name="comment"
-          placeholder="Add a new comment"
-          cols="30"
-          rows="5"
-          className="conversations__message"
-          autofocus
-          required="required"
-        ></textarea>
-        <div className="conversations__btn">
-          <Button>
-            <img
-              className="conversations__icon"
-              src={commentIcon}
-              alt="upload icon"
-            />
-            <p className="conversations__content">comment</p>
-          </Button>
+        </p>
+        <div className="conversations__wrapper">
+          <textarea
+            id="message"
+            name="comment"
+            placeholder="Add a new comment"
+            cols="30"
+            rows="1"
+            className="conversations__message"
+            autofocus
+            required="required"
+          ></textarea>
+          <div className="conversations__btn">
+            <Button>
+              <img
+                className="conversations__icon"
+                src={commentIcon}
+                alt="upload icon"
+              />
+              <p className="conversations__content">comment</p>
+            </Button>
+          </div>
         </div>
       </div>
     </form>
